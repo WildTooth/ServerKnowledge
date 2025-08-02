4ef0a1a1aafbfcfb1981ca5ef7e15a0bb27623ed
WildTooth <champenv@outlook.dk>
2025-08-02 03:10:12 +0200

    Add CuboidRegion class for defining 3D spatial regions with min and max points

---
+++ api/src/main/kotlin/com/github/wildtooth/knowledge/regions/CuboidRegion.kt
@@ -0,0 +1,27 @@
+package com.github.wildtooth.knowledge.regions
+
+import com.github.wildtooth.knowledge.math.BlockVector3
+
+class CuboidRegion : Region {
+
+  constructor(
+    id: String,
+    displayName: String,
+    weight: Int,
+    min: BlockVector3,
+    max: BlockVector3
+  ) : super(id, displayName, weight) {
+    this.setMinMaxPoints(min, max)
+  }
+
+  private fun setMinMaxPoints(min: BlockVector3, max: BlockVector3) {
+    this.setMinMaxPoints(listOf(min, max))
+  }
+
+  override fun contains(point: BlockVector3): Boolean {
+    val x: Int = point.x
+    val y: Int = point.y
+    val z: Int = point.z
+    return (((x >= min!!.x) && (x < max!!.x + 1)) && ((y >= min!!.y) && (y < max!!.y + 1)) && ((z >= min!!.z) && (z < max!!.z + 1)))
+  }
+}
